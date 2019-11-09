ed45d48963dd5ca5256257df343c76c7616f5343
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-11-08 18:08:18 -0800

    ReactDOM not working.  Login/Logout button change DOES work :)

--- Helpdesk-Ticketing/ClientApp/src/components/NavMenu.js
+++ Helpdesk-Ticketing/ClientApp/src/components/NavMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
@@ -8,6 +9,10 @@ export class NavMenu extends Component {
 
   constructor (props) {
     super(props);
+
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
@@ -21,12 +26,28 @@ export class NavMenu extends Component {
     });
   }
 
+  handleLoginClick() {
+      this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+      this.setState({ isLoggedIn: false });
+  }
+
   render () {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+        button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+        button = <LoginButton onClick={this.handleLoginClick} />;
+    }
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">Helpdesk_Ticketing</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Helpdesk Ticketing System</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
@@ -45,6 +66,11 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/login-view">User Login</NavLink>
                 </NavItem>
+
+                <div>
+                  <Greeting isLoggedIn={isLoggedIn} />
+                  {button}
+                </div>
               </ul>
             </Collapse>
           </Container>
@@ -53,3 +79,41 @@ export class NavMenu extends Component {
     );
   }
 }
+
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign in user.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+    </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+    </button>
+    );
+}
+/*
+ReactDOM.render(
+    <NavMenu />,
+    document.getElementById('root')
+);
+*/
