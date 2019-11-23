fc4ea0b0206907c94587810128e2f2e03faf6aea
castillocarlosr <castillocarlosr@gmail.com>
2019-11-22 18:57:31 -0800

    added models

---
+++ WithAuth/Models/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WithAuth.Models
+{
+    public class Inventory
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public Difficulty GetDifficulty { get; set; }
+
+        public enum Difficulty
+        {
+            Easy,
+            Medium,
+            Hard
+        }
+    }
+}
+
