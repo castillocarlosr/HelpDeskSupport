f4a16d419de7bc13c97c998c1fd5614a6c89633f
castillocarlosr <castillocarlosr@gmail.com>
2019-11-22 18:05:56 -0800

    changed login react

--- WithAuth/ClientApp/src/components/NavMenu.js
+++ WithAuth/ClientApp/src/components/NavMenu.js
@@ -32,13 +32,13 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Home &nbsp;&nbsp;</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Simple Counter</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/counter">Simple Counter &nbsp;&nbsp;</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data &nbsp;&nbsp;</NavLink>
                 </NavItem>
                 <LoginMenu>
                 </LoginMenu>
